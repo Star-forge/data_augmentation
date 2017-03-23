51a969772203935e41373ee7ab3659f7d06b8f9b
starforge <mail.starforge@gmail.com>
2017-03-23 07:10:08 +0300

    add blur

--- img_augmentation.py
+++ img_augmentation.py
@@ -5,23 +5,34 @@ import os
 import numpy as np
 
 
-from PIL import Image
+from PIL import Image, ImageFilter
+
+def init():
+    # задаются параметры приложения
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-d", "--dir", required=True, help="Директория поиска изображений")
+    ap.add_argument("-n", "--noise", required=False,
+                    help="Добавление шума: 1 - добавлять шум к изображению при вращении; пустая строка "" - не добавлять")
+    return vars(ap.parse_args())
+
+def get_image_from_path(directory, image_name):
+    img_path = directory + os.sep + image_name;
+    # Чтение изображения
+    im = Image.open(img_path).convert("RGBA")
+    return im
 
 def get_noise_image(width, height):
     imarray = np.random.rand(height, width, 3) * 255
     im = Image.fromarray(imarray.astype('uint8')).convert('RGBA')
     return im
 
-def modify_image(directory, image, isNoiseFill):
+def modify_image(im, isNoiseFill):
     filled = ""
-    img_path = directory+os.sep+image;
     # Директория для результатов
     output_directory = directory + os.sep + "out"
     # Если такой директории нет - создание
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
-    # Чтение изображения
-    im = Image.open(img_path).convert("RGBA")
 
     # Вращение изображения от 2 до 358 градусов с шагом 2(50)
     for k in range(2, 358, 50):
@@ -41,24 +52,28 @@ def modify_image(directory, image, isNoiseFill):
 
 # входная точка приложения
 if __name__ == "__main__":
-    # задаются параметры приложения
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-d", "--dir", required=True, help="Директория поиска изображений")
-    ap.add_argument("-n", "--noise", required=False, help="Добавление шума: 1 - добавлять шум к изображению при вращении; пустая строка "" - не добавлять")
-    args = vars(ap.parse_args())
-
+    #
+    args = init()
     # получение значения параметра "Директория поиска изображений"
     directory = args["dir"]
     # получение значения параметра "Добавление шума"
     noise = args["noise"]
     if noise == None: noise = ""
-    #Получаем список файлов в переменную files
+    # Получение списка файлов в переменную files
     files = os.listdir(directory)
-    #Фильтруем список
+    # Фильтрация списка, по расширениям '.jpg' или '.gif'
     images = filter(lambda x: x.endswith('.jpg' or '.gif'), files)
+
+    #
+    # растягивание и сжатие по осям X и Y на определенное расстояние,
+    # сдвиги,
+    # введение на фото бликов и линий,
+    # дефокус.
 
     print("Начало обработки в директории: "+ directory + ", генерация шума: " + noise)
     # запуск обработки каждого из изображений
-    for image in images:
-        print(image)
-        modify_image(directory, image, noise)
+    for image_name in images:
+        print(image_name)
+        im = get_image_from_path(directory, image_name)
+        modify_image(im, noise)
+        blurred_image = im.filter(ImageFilter.GaussianBlur(radius=5))
