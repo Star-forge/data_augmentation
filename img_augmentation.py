1fb74133d13a587cf427f1b3fd58e5bf91ec5134
Andrei Demakov <mail.starforge@gmail.com>
2017-04-12 02:57:51 +0300

    Update img_augmentation.py
    add colorful lines and glare

--- img_augmentation.py
+++ img_augmentation.py
@@ -65,6 +65,14 @@ def init():
         default="3",
         help="Количество линий (по-умолчанию: 3)"
     )
+    ap.add_argument(
+        "-g",
+        "--glare",
+        type=int,
+        required=False,
+        default="1",
+        help="Количество бликов (по-умолчанию: 1)"
+    )
     return vars(ap.parse_args())
 
 def get_image_from_path(in_dir, image_name):
@@ -73,14 +81,15 @@ def get_image_from_path(in_dir, image_name):
     return im
 
 # * Сохранение изображения в файл с определенным именем
-def save_image_to_path(image, output_directory, image_name):
+def save_image_to_path(image, output_directory, image_name, resize=True):
     # Если директории для сохранения нет - создание
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
     # Получение полного пути к изображению, включающего директорию и имя файла
     image_path = output_directory + image_name
     # Изменение размера
-    image = image.resize(IMAGE_SIZE, Image.LANCZOS)
+    if resize :
+        image = image.resize(IMAGE_SIZE, Image.LANCZOS)
     # Запись в файл
     image.save(image_path)
 
@@ -116,14 +125,83 @@ def stretch_image(input_directory, output_directory, image_name, X):
 # * Введение на изображение бликов и линий
 def addLines(input_directory, output_directory, im_name, lines_num):
     im = get_image_from_path(input_directory, im_name)
-    # новое имя файла в директории для результатов = изначальное-имя_rotated_угол-вращения.изначальное-расширение
+    # новое имя файла в директории для результатов
     draw = ImageDraw.Draw(im)
     img_x, img_y = im.size
     for i in range(0,lines_num):
         line_image_name = im_name[:im_name.rindex('.')] + "_l" + str(i) + splitext(im_name)[1]
-        draw.line((random.randint(0, img_x), random.randint(0, img_y), random.randint(0, img_x), random.randint(0, img_y)), fill=128, width=10)
+        r = lambda: random.randint(0, 255)
+        color = ('#%02X%02X%02X' % (r(), r(), r()))
+        draw.line((random.randint(0, img_x), random.randint(0, img_y), random.randint(0, img_x), random.randint(0, img_y)), fill=color, width=10)
         save_image_to_path(im, output_directory, line_image_name)
 
+def brightnessUP(im):
+    width, height = im.size
+    factor = 50
+    pix = im.load()
+    draw = ImageDraw.Draw(im)
+    for i in range(width):
+        for j in range(height):
+            a = pix[i, j][0] + factor
+            b = pix[i, j][1] + factor
+            c = pix[i, j][2] + factor
+            if (a < 0):
+                a = 0
+            if (b < 0):
+                b = 0
+            if (c < 0):
+                c = 0
+            if (a > 255):
+                a = 255
+            if (b > 255):
+                b = 255
+            if (c > 255):
+                c = 255
+            draw.point((i, j), (a, b, c))
+    return im
+
+def drawGlare(im, AX, AY, BX, BY):
+    # print("glare", AX, AY, BX, BY)
+    center_left = int(AX + (BX - AX) / 2 - (BX - AX) / 10)
+    center_top = int(AY + (BY - AY) / 2 - (BY - AY) / 10)
+    center_right = int(AX + (BX - AX) / 2 + (BX - AX) / 10)
+    center_bottom = int(AY + (BY - AY) / 2 + (BY - AY) / 10)
+    step = 15
+    for i in range(step, 60, step):
+        left = int(AX+(BX-AX)/2-(BX-AX)/100*i)
+        top = int(AY+(BY-AY)/100*i-(BY-AY)/100*step)
+        right = int(AX+(BX-AX)/2+(BX-AX)/100*i)
+        bottom = int(BY-(BY-AY)/100*i+(BY-AY)/100*step)
+        cropped_im = im.crop((left, top, right, bottom))
+        # save_image_to_path(cropped_im, IN_DIR_ROOT + os.sep + ".." + os.sep, "tmp.jpg", False)
+        # cropped_im = get_image_from_path(IN_DIR_ROOT + os.sep + ".." + os.sep, "tmp.jpg")
+        # cropped_im = cropped_im.filter(ImageFilter.BLUR)
+        brightnessUP(cropped_im)
+        # cropped_im.show()
+        im.paste(cropped_im, (left, top))
+    cropped_im = im.crop((center_left, center_top, center_right, center_bottom))
+    # save_image_to_path(cropped_im, IN_DIR_ROOT + os.sep + ".." + os.sep, "tmp.jpg", False)
+    # cropped_im = get_image_from_path(IN_DIR_ROOT + os.sep + ".." + os.sep, "tmp.jpg")
+    cropped_im = cropped_im.filter(ImageFilter.BLUR)
+    im.paste(cropped_im, (center_left, center_top))
+    return im
+
+def addGlare(input_directory, output_directory, im_name, glare_num):
+    im = get_image_from_path(input_directory, im_name)
+    # новое имя файла в директории для результатов
+    draw = ImageDraw.Draw(im)
+    img_x, img_y = im.size
+    for i in range(0, glare_num):
+        glare_image_name = im_name[:im_name.rindex('.')] + "_G" + str(i) + splitext(im_name)[1]
+        # Координаты точек в которые будет вписан круг (размером не более чем 1/3 всего изображения)
+        AX = random.randint(int(img_x/5), img_x-int(img_x / 3))
+        AY = random.randint(int(img_x/5), img_y-int(img_y / 3))
+        circle_diameter = random.randint(int(img_x / 4), int(img_y / 3))
+        BX = AX+circle_diameter
+        BY = AY+circle_diameter
+        # draw.ellipse((AX, AY, BX, BY), outline='silver')
+        im = drawGlare(im, AX, AY, BX, BY)
+        save_image_to_path(im, output_directory, glare_image_name)
 
 # * Блюр(дефокус) изображений (Размытие по Гауссу с радиусом (radius))
 def blur_image(input_directory, output_directory, im_name, radius):
@@ -163,6 +241,12 @@ def GetListForAugmentation(ImagePath):
         if not os.path.isdir(path):
             if (path.endswith(tuple(EXTENSIONS))):
                 ImageList.append(path)
+            else:
+                img = Image.open(path)
+                img_new = splitext(path)[0]+".jpg"
+                img.save(img_new)
+                ImageList.append(img_new)
+
         else:
             ImageList += GetListForAugmentation(path)
     return ImageList
@@ -196,16 +280,20 @@ if __name__ == "__main__":
     radius = args["radius"]
     # получение значения параметра "Количество линий"
     lines_num = args["lines"]
+    # получение значения параметра "Количество линий"
+    glare_num = args["glare"]
 
-    if (radius > 3 | radius <= 0): radius = 3
-    if (lines_num > 3 | lines_num <= 0): lines_num = 3
+    if (radius <= 0): radius = 3
+    if (lines_num <= 0): lines_num = 3
+    if (glare_num <= 0): glare_num = 1
 
     print("Директория поиска изображений:\t" + str(input_directory) +
           "\nУгол поворота:   \t\t" + str(angle) +
           "\nГраницы сектора: \t\t" + str(sector) +
           "\nГенерация шума:  \t\t" + str(noise) +
           "\nРадиус размытия: \t\t" + str(radius) +
-          "\nКоличество линий:\t\t" + str(lines_num))
+          "\nКоличество линий:\t\t" + str(lines_num) +
+          "\nКоличество бликов:\t\t" + str(glare_num))
 
     print("\n***ПОИСК ФАЙЛОВ***")
     image_list = GetListForAugmentation(input_directory)
@@ -249,13 +337,15 @@ if __name__ == "__main__":
 
         # Директория для результатов вращения
         OUT_DIR = OUT_DIR_ROOT + os.sep + "#R" + os.sep + IN_DIR[len(IN_DIR_ROOT):]
-        print("[" + str(step).zfill(len(str(len(image_list)))) + "/" + str(len(image_list)) + "] Rotate:\t" + IN_DIR +"\t"+ IMAGE_NAME +"\t"+ OUT_DIR)
+        print("[" + str(step).zfill(len(str(len(image_list)))) + "/" + str(
+            len(image_list)) + "] Rotate:\t" + IN_DIR + "\t" + IMAGE_NAME + "\t" + OUT_DIR)
         # Вращение изображения
         rotate_image(IN_DIR, OUT_DIR, IMAGE_NAME, angle, sector, noise)
 
         # Директория для результатов внесения дефокуса
         OUT_DIR = OUT_DIR_ROOT + os.sep + "#B" + os.sep + IN_DIR[len(IN_DIR_ROOT):]
-        print("[" + str(step).zfill(len(str(len(image_list)))) + "/" + str(len(image_list)) + "] Blur:   \t" + IN_DIR +"\t"+ IMAGE_NAME +"\t"+ OUT_DIR)
+        print("[" + str(step).zfill(len(str(len(image_list)))) + "/" + str(
+            len(image_list)) + "] Blur:   \t" + IN_DIR + "\t" + IMAGE_NAME + "\t" + OUT_DIR)
         # Дефокус (размытие)
         blur_image(IN_DIR, OUT_DIR, IMAGE_NAME, radius)
 
@@ -266,12 +356,22 @@ if __name__ == "__main__":
         # Линии
         addLines(IN_DIR, OUT_DIR, IMAGE_NAME, lines_num)
 
+        # Директория для результатов рисования бликов
+        OUT_DIR = OUT_DIR_ROOT + os.sep + "#G" + os.sep + IN_DIR[len(IN_DIR_ROOT):]
+        print("[" + str(step).zfill(len(str(len(image_list)))) + "/" + str(
+            len(image_list)) + "] Lines:   \t" + IN_DIR + "\t" + IMAGE_NAME + "\t" + OUT_DIR)
+        # Блики
+        addGlare(IN_DIR, OUT_DIR, IMAGE_NAME, glare_num)
+
         # stretch_image(input_directory, output_directory, basename(image_name), 100)
 
     print("\n***ОБРАБОТКА ЗАВЕРШЕНА***")
-    print("Новый датасет находится по пути = " + OUT_DIR_ROOT +
-          ". В директории '#R' - повернутые изображения, в директории '#B' - размытые, а в директории '#L' - с линиями."
-          "Все изображения приведены к размеру " +str(IMAGE_SIZE)+ " пикселей (с помощью фильтра Ланцоша). "
+    print("Новый датасет находится по пути = " + OUT_DIR_ROOT + "." +
+          "\nВ директории '#R' - повернутые изображения, "+
+          "\nв директории '#L' - с линиями, "+
+          "\nв директории '#G' - с бликами, "+
+          "\nа в директории '#B' - размытые. "+
+          "\nВсе изображения приведены к размеру " +str(IMAGE_SIZE)+ " пикселей (с помощью фильтра Ланцоша). "+
           "\nКоличество файлов в первоначальном датасете = " + str(FILES_COUNT) +
           "\nКоличество файлов в итоговом датасете = " + str(len(GetListForAugmentation(OUT_DIR_ROOT))))
     print("\n")
